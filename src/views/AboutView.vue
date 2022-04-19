8e3811a62d145ff1300c92ae06ef443f09323195
marocks31 <mollyarisley@gmail.com>
2022-04-18 19:50:02 -0500

    mapbox api and made access token into environment variable

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="map"></div>
   </div>
 </template>
+
+<script>
+/* global mapboxgl */
+export default {
+  data: function () {
+    return {};
+  },
+  mounted: function () {
+    mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-87.6298, 41.8781], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    console.log(map);
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  height: 300px;
+  width: 100%;
+}
+</style>
